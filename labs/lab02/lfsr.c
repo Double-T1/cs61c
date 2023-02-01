484e718141d6a85c8d1a8a18f98d34bc0771ecf2
chen weichen <newsforcwc@gmail.com>
2023-02-01 17:03:23 +0800

    lab2 done

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -6,5 +6,11 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t zero_bit = 1 & *reg;
+    uint16_t second_bit = 1 & (*reg>>2);
+    uint16_t third_bit = 1 & (*reg>>3);
+    uint16_t fifth_bit = 1 & (*reg>>5);
+    (*reg) >>= 1;
+    *reg |= ((zero_bit ^ second_bit ^ third_bit ^ fifth_bit) << 15);
 }
 
