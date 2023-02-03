4a302e70600da32299d5eef72f0fa88add044ca6
chen weichen <newsforcwc@gmail.com>
2023-02-03 17:39:26 +0800

    lab3 done

--- labs/lab03/factorial.s
+++ labs/lab03/factorial.s
@@ -20,5 +20,21 @@ main:
     addi a0, x0, 10
     ecall # Exit
 
+#recursive version
 factorial:
     # YOUR CODE HERE
+    # n is stored at a0
+    li t0, 1
+    beq a0, t0, finish #if (n == 1) jump to finish
+    addi sp, sp, -8 #expand the stack frame
+    sw ra, 4(sp) #push ra onto stack
+    sw a0, 0(sp) #store the current n
+    addi a0, a0, -1 #prep n-1
+    jal ra, factorial #call factorial(n-1), ra will be stored with the current address
+    lw t0, 0(sp)
+    mul a0, a0, t0
+    lw ra, 4(sp)
+    addi sp, sp, 8 #delete the stack frame
+finish:
+    ret
+    
